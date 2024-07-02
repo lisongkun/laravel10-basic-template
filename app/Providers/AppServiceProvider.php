1bcee1f3b86c9b9534a5e75e9307cbfc1679aa4f
tuyue <hr@tuyuetech.com>
2024-07-02 14:29:15 +0800

    Feat: Enable automatic SQL query logging in development environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \Schema::defaultStringLength(191);
+
+        // 开发环境打印 SQL
+        if (config('app.env') === 'local') {
+            \DB::listen(function ($query) {
+                $sql = $query->sql;
+                $bindings = $query->bindings;
+                $time = $query->time;
+                $sql = str_replace('?', "'%s'", $sql);
+                $fullSql = vsprintf($sql, $bindings);
+                \Log::debug('SQL', ['sql' => $fullSql, 'time' => $time]);
+            });
+        }
     }
 }
